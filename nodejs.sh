a00af2a9843322a2acbd3396b657f99a94a981a5
Aziz <mailter@mail.ru>
2023-07-20 14:14:49 +0300

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,11 +1,5 @@
-NODE_VERSION=16.13.0
-apt install -y curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-NVM_DIR=/root/.nvm
-. /root/.nvm/nvm.sh && nvm install ${NODE_VERSION}
-. /root/.nvm/nvm.sh && nvm use v${NODE_VERSION}
-. /root/.nvm/nvm.sh && nvm alias default v${NODE_VERSION}
-PATH="/root/.nvm/versions/node/v${NODE_VERSION}/bin/:${PATH}"
-node --version
-npm --version
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm list-remote
+nvm install v16
 npm i -g pm2
