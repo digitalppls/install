2dcbe1733cf123bbf00be4015c74fae3986245b3
Aziz <mailter@mail.ru>
2024-02-17 18:04:56 +0300

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -7,8 +7,8 @@ else
     echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
     sudo apt-get update
     sudo apt-get install libssl1.1
-    wget -qO - https://www.mongodb.org/static/pgp/server-7.5.asc | sudo apt-key add -
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.5 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.5.list
+    wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
     sudo apt update
     sudo apt install -y mongodb-org
     sudo systemctl enable mongod
